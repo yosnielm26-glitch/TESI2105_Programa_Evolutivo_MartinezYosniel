78f3b33a37d03a67185639cfea7f50f8abbe029a
yosnielm26-glitch <yosnielm26@gmail.com>
2026-09-09 17:18:18 +0000

    Version 2 Planificador

--- Planificacion_de_Actividades.py
+++ Planificacion_de_Actividades.py
@@ -1,14 +1,26 @@
-# Planificador de actividades sencillo
-
-actividad = input('Nombre de la actividad: ')
-minutos = int(input('Minutos por actividad: '))
-veces = int(input('Veces que la haras al dia: '))
+# Planificador de actividades - Version 2.0
 
 PREPARACION = 5
+continuar = 'si'
 
-subtotal = minutos * veces
-total = subtotal + PREPARACION
+while continuar == 'si':
+    actividad = input('Nombre de la actividad: ')
+    minutos = int(input('Minutos por actividad: '))
+    veces = int(input('Veces que la haras al dia: '))
 
-print('Tarea:', actividad)
-print('Minutos de trabajo:', subtotal)
-print('Total de minutos con preparacion:', total)
+    subtotal = minutos * veces
+    total = subtotal + PREPARACION
+
+    print('--- Resumen ---')
+    print('Tarea:', actividad)
+    print('Total de minutos con preparacion:', total)
+
+    if total > 60:
+        print('Aviso: Requiere mas de 1 hora de tu dia.')
+    else:
+        print('Aviso: Actividad corta, facil de planificar.')
+
+    print('----------------')
+    continuar = input('Quieres planificar otra actividad? (si/no): ')
+
+print('Gracias por usar el planificador.')
